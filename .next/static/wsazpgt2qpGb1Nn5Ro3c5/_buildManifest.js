3a2ab77fbe7b9083463e52348233bd7557388e49
wangxin <wangxin@huitongjy.com>
2023-02-15 10:16:53 +0800

    add readme

---
+++ .next/static/wsazpgt2qpGb1Nn5Ro3c5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/253-887a0e44f21efdc2.js","static/chunks/pages/index-09fd5f8c3a9c845c.js"],"/_error":["static/chunks/pages/_error-dfcfa5bb62767c20.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
